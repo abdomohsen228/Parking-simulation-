3920a00859ddf7e3bcaf707199f39459e4a37c72
MZiyad24 <122838772+MZiyad24@users.noreply.github.com>
2024-11-09 03:11:18 +0200

    semaphore integrity fixed, some output specification and comments

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -6,6 +6,7 @@ class Car implements Runnable {
     private final int parkingDuration;
     private final int gateId;
     private final ParkingProcess parkingSystem;
+    private int waitingDuration=0;
 
     public Car(int carId, int arrivalTime, int parkingDuration, int gateId, ParkingProcess parkingSystem) {
         this.carId = carId;
@@ -13,28 +14,31 @@ class Car implements Runnable {
         this.parkingDuration = parkingDuration;
         this.gateId = gateId;
         this.parkingSystem = parkingSystem;
+        waitingDuration=0;
     }
 
     @Override
     public void run() {
         try {
-            Thread.sleep(arrivalTime * 1000L);
-            System.out.println("Car " + carId + " from Gate " + gateId + " arrived at time " + arrivalTime);
+            Thread.sleep(arrivalTime * 1000L); // duration of staying
+            System.out.println("Car " + carId + " from Gate " + gateId + " arrived at time " + arrivalTime); // car status
             synchronized (parkingSystem) {
                 // not a bust wait because parkingSystem.wait(); make thread ideal
-                while (ParkingProcess.getParkingLot().availablePermits() == 0) {
+                while (ParkingProcess.getParkingLot().availablePermits() == 0) {  // if no place available
                     System.out.println("Car " + carId + " from Gate " + gateId + " waiting for a spot.");
+                    waitingDuration++;
                     parkingSystem.wait();
                 }
-                ParkingProcess.getParkingLot().acquire();
+                // ParkingProcess.getParkingLot().acquire(); // if there is an empty place
                 parkingSystem.carEntered(gateId);
-                System.out.println("Car " + carId + " from Gate " + gateId + " parked. (Parking Status: "
+                System.out.println("Car " + carId + " from Gate " + gateId + " parked after waiting for " + waitingDuration +" units of time. (Parking Status: "
                         + parkingSystem.getCurrentCarsParked() + " spots occupied)");
             }
             Thread.sleep(parkingDuration * 1000L);
+            // case when cars exit
             synchronized (parkingSystem) {
                 parkingSystem.carExited();
-                ParkingProcess.getParkingLot().release();
+                // ParkingProcess.getParkingLot().release(); // emptying place in the semaphore
                 System.out.println("Car " + carId + " from Gate " + gateId + " left after " + parkingDuration + " units of time. (Parking Status: "
                         + parkingSystem.getCurrentCarsParked() + " spots occupied)");
                 parkingSystem.notifyAll();
--- src/main/java/org/example/ParkingProcess.java
+++ src/main/java/org/example/ParkingProcess.java
@@ -7,25 +7,28 @@ import java.util.Map;
 
 class ParkingProcess {
     private static final int SPOTS = 4;
-    private static final Semaphore parkingLot = new Semaphore(SPOTS, true);
+    private static final Semaphore parkingLot = new Semaphore(SPOTS, true); // queue for 4 spots ordered
     private int currentParked = 0;
     private int totalServed = 0;
     private final Map<Integer, AtomicInteger> gateStats = new HashMap<>();
 
     public ParkingProcess() {
-        gateStats.put(1, new AtomicInteger(0));
+        gateStats.put(1, new AtomicInteger(0));// gate number , number of entered cars
         gateStats.put(2, new AtomicInteger(0));
         gateStats.put(3, new AtomicInteger(0));
     }
 
-    public synchronized void carEntered(int gateId) {
+    public synchronized void carEntered(int gateId) throws InterruptedException {
+        parkingLot.acquire();
         currentParked++;
         totalServed++;
         gateStats.get(gateId).incrementAndGet();
+
     }
 
     public synchronized void carExited() {
         currentParked--;
+        parkingLot.release();
     }
 
     public synchronized int getCurrentCarsParked() {
